7b1a39ef78cde0b343e31403b45826b1030d63b1
Shiryee Lin <908087650@QQ.com>
2021-09-19 09:33:44 +0800

    Initial

---
+++ scripts/lean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+pushd package/lean
+git clone --depth=1 https://github.com/fw876/helloworld
+rm -rf luci-theme-argon
+rm -rf luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone -b master https://github.com/jerrykuku/luci-app-argon-config.git
+popd
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
